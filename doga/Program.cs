ad56c52d19c817f9b30a2fd717264fd87bfee102
Marcell Rabotka <rigbimarci@gmail.com>
2023-12-18 12:24:19 +0100

    3.feladat lista kiírása vesszőkkel

--- doga/Program.cs
+++ doga/Program.cs
@@ -14,13 +14,17 @@ namespace doga
             List<int> list = new List<int>();
             while (szam!=0)
             {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.Write($"{list[i]},");
+                }
+                Console.WriteLine();
                 Console.WriteLine("Kérek egy számot, Vége:0");
                 szam = Convert.ToInt32(Console.ReadLine());
                 if (szam != 0)
                 {
                     list.Add(szam);
-                }
-               
+                }         
             }
             
             Console.ReadKey();
